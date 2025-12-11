d28eb56f66993ba330a2fed727bc9603aca053dd
Baye Talla <bmorthiam@gmail.com>
2025-12-11 11:00:06 +0000

    ajout du click avec confirmation

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ let tabtr = document.querySelectorAll("tbody tr");
 // Ecouteur d'evenement sur les tr
 for (const tr of tabtr) {
   tr.addEventListener("click", function () {
-    tr.classList.toggle("gris");
+    this.classList.toggle("gris");
   });
 }
+//
+let lien = document.querySelector("a");
+lien.addEventListener("click", function (e) {
+  e.preventDefault();
+  let target = this.getAttribute("href");
+  let response = confirm("Vous allez partir du le site");
+  if (response == true) {
+    window.location = target; // diréction vers un autre site
+  } else {
+    alert("Merci de rester avec nous");
+  }
+});
