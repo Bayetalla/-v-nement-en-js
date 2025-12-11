135d2e77f0f1d73c6d0b049cd7dfc7827b603455
Baye Talla <bmorthiam@gmail.com>
2025-12-11 09:46:27 +0000

    ajout d'ecouteur d'evenement sur tous  les tr

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  let trs = document.querySelectorAll("tbody tr");
-  console.log("Nombre de lignes trouvées:", trs.length);
-
-  trs.forEach((tr) => {
-    tr.addEventListener("click", function () {
-      console.log("Ligne cliquée:", this);
-      this.classList.toggle("gris");
-      console.log("Classes après toggle:", this.classList);
-    });
+// Selectionner tous les elements de tr
+let tabtr = document.querySelectorAll("tbody tr");
+// Ecouteur d'evenement sur les tr
+for (const tr of tabtr) {
+  tr.addEventListener("click", function () {
+    tr.classList.toggle("gris");
   });
-});
+}
